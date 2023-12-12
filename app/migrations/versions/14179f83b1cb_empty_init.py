5d2016e8d791d834f8eaa5855dc885f4eabedc85
Markswell <osoromarkswell80@gmail.com>
2023-12-12 18:23:59 +0300

    Assignment Complete

---
+++ app/migrations/versions/14179f83b1cb_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 14179f83b1cb
+Revises: 
+Create Date: 2023-12-12 18:05:56.359406
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '14179f83b1cb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
